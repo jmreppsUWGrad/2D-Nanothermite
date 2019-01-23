b68672dd1582a1c44578b976b24ee8819171d681
jmrepps <jmrepps@gitlab.uwaterloo.ca>
2019-01-23 15:37:18 -0500

    More testing

--- Post-processing.py
+++ Post-processing.py
@@ -129,7 +129,7 @@ print('#              Created by J. Mark Epps               #')
 print('#          Part of Masters Thesis at UW 2018-2020    #')
 print('######################################################\n')
 
-os.chdir('Tests/dH/2')
+os.chdir('Tests/4')
 
 # Get Arrhenius parameters
 A0=-1.0
