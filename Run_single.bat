6afcf2d536dd46f36d18771868f50ca467903e68
jmrepps <jmrepps@gitlab.uwaterloo.ca>
2019-03-19 18:07:45 -0400

    Extend test to find ignition (not found)

--- Run_single.bat
+++ Run_single.bat
@@ -1,8 +1,8 @@
 @echo OFF
 
-set fold=Tests\Conserv\AlMoO3\18
+set fold=Tests\Conserv\AlMoO3\16
 
-python main.py Input_file18.txt %fold%
+python main.py Input_file16.txt %fold%
 python Post-processing.py %fold%
 
 cd C:\Users\mepps\Documents\Research\2D_Conduction
