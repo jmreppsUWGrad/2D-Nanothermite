6b41b4e95f7f1cf33b63e8dbcc501a6e5df510f4
jmrepps <jmrepps@gitlab.uwaterloo.ca>
2019-05-17 18:26:09 -0400

    Update for mpi

--- Run_batch.bat
+++ Run_batch.bat
@@ -1,14 +1,15 @@
 @echo OFF
 
+set proc=6
 set fold1=Tests\Axisymmetric\AlMoO3\13
 set fold2=Tests\Axisymmetric\AlMoO3\14
 set fold3=Tests\Axisymmetric\AlMoO3\15
 
-REM python main.py Input_file13.txt %fold1%
-python Post.py %fold1% 1
+REM mpiexec -n %proc% python main.py Input_file13.txt %fold1%
+python Post.py %fold1% 0
 
-REM python main.py Input_file14.txt %fold2%
-python Post.py %fold2% 1
+REM mpiexec -n %proc% python main.py Input_file14.txt %fold2%
+python Post.py %fold2% 0
 
-REM python main.py Input_file15.txt %fold3%
-python Post.py %fold3% 1
+REM mpiexec -n %proc% python main.py Input_file15.txt %fold3%
+python Post.py %fold3% 0
--- Run_single.bat
+++ Run_single.bat
@@ -1,10 +1,11 @@
 @echo OFF
 
+set proc=6
 set fold=Tests\Conserv\AlMoO3\16\NoClipping
 REM set fold=Tests\Axisymmetric\AlMoO3\2
 
-REM python main.py Input_file16.txt %fold%
-python main.py Input_file_stats.txt %fold%
+REM mpiexec -n %proc% python main.py Input_file16.txt %fold%
+mpiexec -n %proc% python main.py Input_file_stats.txt %fold%
 python Post.py %fold% 0
 
 cd C:\Users\mepps\Documents\Research\2D_Conduction
